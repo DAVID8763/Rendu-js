8fd16aee0b20d0f7ed8264448281998ababe2fb3
DAVID8763 <DAVID.BENHAMOU@latoile.em-lyon.com>
2025-12-17 11:51:04 +0100

    rendu js

--- script.js
+++ script.js
@@ -10,4 +10,41 @@
 // Etappe 4 
 
 //fetch button
-const h1 document./
+const avantage1 = document.getElementById("Avantage");
+
+const shop1 = document.getElementById("Shop");
+
+const temoignage1 = document.getElementById("Temoignage");
+
+const services = document.getElementById("Services");
+
+
+const API_URL = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/restaurant-bresilien.json"
+
+
+
+
+fetch(API_URL)
+.then(reponse => reponse.json())
+.then(data => {
+    console.log('Données récupées avec succès :', data);
+
+    console.log(data.produits);
+    
+
+    displayAvantage(data);
+    displayShop(data);
+    displayTemoignage(data);
+    displayServices(data);
+})
+
+
+function displayAvantage(data) {
+    let avantages = data.avantagesclients;
+    console.log(avantage);
+
+    avantages.forEach(avantage => {
+        
+    });
+    
+}
